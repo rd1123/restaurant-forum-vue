b636041833ec5668cc3d8f828eee76b0d24ab48f
rd <rdding1123@gmail.com>
2020-08-20 16:51:56 +0800

    初始化restaurant-forum-vue專案

---
+++ src/router/index.js
@@ -0,0 +1,30 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import SignIn from '../views/SignIn.vue'
+import NotFound from '../views/NotFound.vue'
+
+Vue.use(VueRouter)
+
+const routes = [
+  {
+    path: '/signin',
+    name: 'sign-in',
+    component: SignIn
+  },
+  {
+    path: '/signup',
+    name: 'sign-up',
+    component: () => import('../views/SignUp.vue')
+  },
+  {
+    path: '*',
+    name: 'not-found',
+    component: NotFound
+  }
+]
+
+const router = new VueRouter({
+  routes
+})
+
+export default router
